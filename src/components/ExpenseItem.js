5dc7f046f67967704e2c18ef9328625ebe212289
SanugondaVijayKumar <myemail>
2023-08-20 21:19:29 +0530

    task 7

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem() {
+  return (
+    // <div>
+    //   <div>Date</div>
+    //   <div>
+    //     <h2>Title</h2>
+    //   </div>
+    //   <div>amount</div>
+    // </div>
+    <div>
+      <div>Food Rs 10</div>
+      <div>Petrol Rs 100</div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;

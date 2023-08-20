317481d945f618babc6cf1155e8a53a53923b2a5
SanugondaVijayKumar <myemail>
2023-08-20 22:12:53 +0530

    task9

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,25 +1,15 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
-  const LocationOfExpenditure = "Mumbai";
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense_item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div>{LocationOfExpenditure}</div>
-      <div className="expense-item__price">${expenseAmount}</div>
+      <div>{props.location}</div>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
-    // <div>
-    //   <div>Food Rs 10</div>
-    //   <div>Petrol Rs 100</div>
-    //   <div>Movies Rs 200</div>
-    // </div>
   );
 }
 

3c7f19d7531d704ab769c96637092a0f841ce52c
SanugondaVijayKumar <myemail>
2023-08-21 16:51:05 +0530

    task 2

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,26 +1,30 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const clickHandler = () => {
-    console.log("clicked!!!");
-  };
+  const [title, setTitle] = useState(props.title);
+  const [amount, setExpense] = useState(props.amount);
 
-  const deleteExpense = () => {
-    console.log("expense got deleted!");
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
+  const changeExpense = () => {
+    setExpense(100);
   };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense_item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div>{props.location}</div>
-      <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item__price">${amount}</div>
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={deleteExpense}>Delete Expense</button>
+      <button onClick={changeExpense}>Change Expense</button>
 
       {/* <ExpenseDetails
         amount={props.amount}
